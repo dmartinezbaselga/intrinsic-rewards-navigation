86d0e3484048b73ff809cda2f6086c425c90bfac
LuziaKn <luzia.knoedler@web.de>
2023-07-19 12:01:59 +0200

    merge real robot

--- crowd_nav/jackal_interface.py
+++ crowd_nav/jackal_interface.py
@@ -1,8 +1,9 @@
 import rospy
 from sensor_msgs.msg import Joy
-from geometry_msgs.msg import PoseStamped
+from geometry_msgs.msg import PoseStamped, Point
 from std_msgs.msg import Float64MultiArray
 import numpy as np
+from visualization_msgs.msg import Marker
 
 class State:
     def __init__(self) -> None:
@@ -37,11 +38,14 @@ class JackalInterface:
         self.max_obstacles = 12
         self.obs_received_ = np.zeros(self.max_obstacles)
         self.obs_subs_ = []
+        self.obs_pub_ = []
         for i in range(self.max_obstacles):
             self.obs_subs_.append(rospy.Subscriber("/obstacle" + str(i+1) + "/path_prediction", Float64MultiArray, self.optitrack_obstacle_callback, queue_size=1))
+            self.obs_pub_.append(rospy.Publisher("/obstacle" + str(i+1) + "/visual", Marker, queue_size=10))
+
         self.first_obstacle_id = -1
         self.dynamic_obstacles = []
-        self.dt = None
+        self.dt = 0.2
         # Need to fix dt and the path prediction topic
 
     def bluetooth_callback(self, msg: Joy):
@@ -84,6 +88,32 @@ class JackalInterface:
             self.post_process_obstacles()
         elif self.first_obstacle_id == -1:
             self.first_obstacle_id = obstacle_id
+        marker = Marker()
+        marker.header.frame_id = "map"
+        marker.header.stamp = rospy.Time.now()
+
+        # set shape, Arrow: 0; Cube: 1 ; Sphere: 2 ; Cylinder: 3
+        marker.type = 0
+        marker.id = obstacle_id
+        start = Point()
+        start.x = obs.x
+        start.y = obs.y
+        marker.points.append(start)
+        end = Point()
+        end.x = msg.data[4]
+        end.y = msg.data[5]
+        marker.points.append(end)
+        # Set the scale of the marker
+        marker.scale.x = 0.4
+        marker.scale.y = 0.4
+        marker.scale.z = 1.0
+
+        # Set the color
+        marker.color.r = 0.0
+        marker.color.g = 1.0
+        marker.color.b = 0.0
+        marker.color.a = 1.0
+        self.obs_pub_[obstacle_id-1].publish(marker)
     
     def goal_callback(self, goal: PoseStamped):
         self.goal.x = goal.pose.position.x
